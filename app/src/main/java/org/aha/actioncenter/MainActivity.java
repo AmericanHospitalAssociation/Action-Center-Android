f0d79126f5f0b7aebd33da739afc5041e1c24015
Vince Davis <vince.davis@me.com>
2015-04-15 08:40:01 -0700

    Added Twitter Fabric Logic

--- app/src/main/java/org/aha/actioncenter/MainActivity.java
+++ app/src/main/java/org/aha/actioncenter/MainActivity.java
@@ -4,13 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "trwxm119rAqWC5KmYedIqv8Xl";
+    private static final String TWITTER_SECRET = "6wXav43Xmw2cwJ6e37kB9gGmaIMKFnBJIQ23JhJqF7kgSgfMkT";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Crashlytics(), new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 
