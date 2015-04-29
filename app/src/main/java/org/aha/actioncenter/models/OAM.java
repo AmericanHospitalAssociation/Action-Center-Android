f087a90d648a4677099dd8f28d95b4d876ff8c49
Vince Davis <vince.davis@me.com>
2015-04-29 08:39:24 -0500

    Added OAM Model

---
+++ app/src/main/java/org/aha/actioncenter/models/OAM.java
@@ -0,0 +1,20 @@
+package org.aha.actioncenter.models;
+
+/**
+ * Created by vince on 4/29/15.
+ */
+public class OAM {
+    public String address_line = "";
+    public String ahaid = "";
+    public String email = "";
+    public String city = "";
+    public String first_name = "";
+    public String last_name = "";
+    public String org_name = "";
+    public String phone = "";
+    public String prefix = "";
+    public String state = "";
+    public String status = "";
+    public String country_code = "";
+    public String zip = "";
+}
